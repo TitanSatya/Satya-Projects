dc40fe0dfdcbe04e91410b0108a5b80ca2de653e
TitanSatya <TitanSatya@github.com>
2018-03-11 22:15:51 +0530

    Add project files.

---
+++ CalculatorFS.Console/Program.fs
@@ -0,0 +1,14 @@
+﻿// Learn more about F# at http://fsharp.org
+// See the 'F# Tutorial' project for more help.
+open System
+
+[<EntryPoint>]
+let main argv = 
+    printfn "Please enter first number"
+    let firstNumber = Console.ReadLine()
+    printfn "Please enter second number"
+    let secondNumber = Console.ReadLine()
+    let sum = Convert.ToInt32( firstNumber )+ Convert.ToInt32(secondNumber)
+    printfn("Sum is %A") sum
+    Console.ReadLine() |> ignore
+    0 // return an integer exit code
