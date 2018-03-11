5083499febb334674a5313de7cdd876d8b1e8893
TitanSatya <TitanSatya@github.com>
2018-03-11 22:43:52 +0530

    Added Different Math ops

--- CalculatorFS.Console/Program.fs
+++ CalculatorFS.Console/Program.fs
@@ -4,11 +4,26 @@ open System
 
 [<EntryPoint>]
 let main argv = 
+    printfn "Please select math operation you want to perform"
+    printfn "1. Addition"
+    printfn "2. Substraction"
+    printfn "3. Multipication"
+    printfn "4. Division"
+    let operation = Console.ReadLine()
     printfn "Please enter first number"
     let firstNumber = Console.ReadLine()
     printfn "Please enter second number"
-    let secondNumber = Console.ReadLine()
-    let sum = Convert.ToInt32( firstNumber )+ Convert.ToInt32(secondNumber)
-    printfn("Sum is %A") sum
+   
+    let _secondNumber = Console.ReadLine()
+    printfn "Processing Result"
+    printfn "----------------------------------------------------------------------"
+    let _result  =
+                if operation = "1" 
+                then      (Convert.ToInt32( firstNumber )+ Convert.ToInt32(_secondNumber))
+  
+                elif operation = "2"
+                then     Convert.ToInt32( firstNumber ) - Convert.ToInt32(_secondNumber)
+                else 0  
+    printfn "Result is %A" _result
     Console.ReadLine() |> ignore
     0 // return an integer exit code
